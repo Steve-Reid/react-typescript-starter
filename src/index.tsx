a86c100880b8728053e1f7be9a3d99251d958005
Steve Reid <steve@appby.design>
2021-06-30 19:00:00 +0100

    fix tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom';
 
 const HelloWorld = () => {
-  return <h1>Hello World</h1>;
+  return <h1>Hello React World</h1>;
 };
 
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
